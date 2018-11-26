139d2ca17595fc874bbd66a4c0f9fa82ac13f524
Ann Fang <32095875+e24033150@users.noreply.github.com>
2018-11-27 00:07:25 +0800

    Add files via upload

---
+++ modbus_tcp_parser.py
@@ -0,0 +1,60 @@
+### python version : 3.6.0
+### operating system : windows 10
+import socket
+import binascii
+from query_function import numbers_to_query_functions
+from response_function import numbers_to_response_functions
+
+# use raw socket as a network sniffer
+def sniff_socket():
+    conn = socket.socket(socket.AF_INET, socket.SOCK_RAW)
+    conn.bind(('127.0.0.1', 0))
+    conn.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    conn.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    return conn
+
+def modbus_query(_data):
+    print ('Transaction ID = %d' % int(_data[:4], 16))
+    print ('Length of Modbus TCP PDU = %d' % (int(_data[8:12],16)-2))
+    print ('Unit ID = %d' % int(_data[12:14],16))
+	#function code and data analysis
+    numbers_to_query_functions(_data[14:16], _data[16:])
+    print ('--------------------------------------------------')
+
+def modbus_response(_data):
+    print ('Transaction ID = %d' % int(_data[:4], 16))
+    print ('Length of Modbus TCP PDU = %d' % (int(_data[8:12],16)-2))
+    print ('Unit ID = %d' % int(_data[12:14],16))
+	#function code and data analysis
+    numbers_to_response_functions(_data[14:16], _data[16:])
+    print ('--------------------------------------------------')
+	
+# transform raw data and judge it
+def process_raw_data(_raw_data):
+    ascii_data = binascii.hexlify(_raw_data).decode('ascii')
+
+    if ascii_data[64:68] == '5018':
+        if ascii_data[44:48]=='01f6':
+            print ('-------------------Modbus Query-------------------')
+            modbus_query(ascii_data[80:])
+        elif ascii_data[40:44]=='01f6':
+            print ('------------------Modbus Response-----------------')
+            modbus_response(ascii_data[80:])
+        else:
+            pass
+    elif ascii_data[64:68] == '5010':
+        print ('ACK Packet')
+    elif ascii_data[64:68] == '8002':
+        print ('SYN Packet')
+    elif ascii_data[64:68] == '8012':
+        print ('SYN/ACK Packet')
+    else:
+        pass
+
+# main function
+if __name__ == "__main__":
+    sniffer = sniff_socket()
+
+    while True:
+        raw_data, addr = sniffer.recvfrom(65565)
+        process_raw_data(raw_data)
---
+++ query_function.py
@@ -0,0 +1,84 @@
+def read_discrete_inputs(_modbus_data):
+    print ('Function name : Read Discrete Inputs')
+
+def read_coils(_modbus_data):
+    print ('Function name : Read Coils')
+
+def write_single_coil(_modbus_data):
+    print ('Function name : Write Single Coil')
+
+def write_multiple_coils(_modbus_data):
+    print ('Function name : Write Multiple Coils')
+
+def read_input_registers(_modbus_data):
+    print ('Function name : Read Input Registers')
+
+def read_multiple_holding_registers(_modbus_data):
+    print ('Function name : Read Multiple Holding Registers')
+    
+def write_single_holding_register(_modbus_data):
+    print ('Function name : Write Single Holding Register')
+	
+def write_multiple_holding_registers(_modbus_data):
+    print ('Function name : Write Multiple Holding Registers')
+	
+def read_write_multiple_registers(_modbus_data):
+    print ('Function name : Read/Write Multiple Registers')
+
+def mask_write_register(_modbus_data):
+    print ('Function name : Mask Write Register')
+
+def read_fifo_queue(_modbus_data):
+    print ('Function name : Read FIFO Queue')
+	
+def read_file_record(_modbus_data):
+    print ('Function name : Read File Record')
+		
+def write_file_record(_modbus_data):
+    print ('Function name : Write File Record')
+	
+def read_exception_status(_modbus_data):
+    print ('Function name : Read Exception Status')
+	
+def diagnostic(_modbus_data):
+    print ('Function name : Diagnostic')
+	
+def get_com_event_counter(_modbus_data):
+    print ('Function name : Get Com Event Counter')
+	
+def get_com_event_log(_modbus_data):
+    print ('Function name : Get Com Event Log')
+	
+def report_slave_id(_modbus_data):
+    print ('Function name : Report Slave ID')
+	
+def read_device_identification_or_others(_modbus_data):
+    print ('Function name : Report Read Device Identification or Others')
+	
+switcher = {
+	    '02': read_discrete_inputs,
+		'01': read_coils,
+		'05': write_single_coil,
+		'0f': write_multiple_coils,
+		'04': read_input_registers,
+		'03': read_multiple_holding_registers,
+		'06': write_single_holding_register,
+		'10': write_multiple_holding_registers,
+		'17': read_write_multiple_registers,
+		'16': mask_write_register,
+		'18': read_fifo_queue,
+		'14': read_file_record,
+		'15': write_file_record,
+		'07': read_exception_status,
+		'08': diagnostic,
+		'0b': get_com_event_counter,
+		'0c': get_com_event_log,
+		'11': report_slave_id,
+		'2b': read_device_identification_or_others
+	}
+	
+def numbers_to_query_functions(argument, modbus_data):
+    # Get the function from switcher dictionary
+    func = switcher.get(argument)
+    # Execute the function
+    func(modbus_data)
